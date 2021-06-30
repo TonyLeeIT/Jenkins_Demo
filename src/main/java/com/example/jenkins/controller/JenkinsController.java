947d9bd9322dd13e07195d1d90338c99ff9ac98b
Tien <tienlm@infoplusvn.com>
2021-06-30 11:19:02 +0700

    init code

---
+++ src/main/java/com/example/jenkins/controller/JenkinsController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello Jenkins";
+    }
+}
